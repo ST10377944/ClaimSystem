d2ac6da93683f5c8a225c2dd18532dc0ac6ce2eb
Sbonakaliso Madlopha <ST10377944@rcconnect.edu.za>
2024-10-25 15:56:48 +0000

    Colour Coding Testing

--- ContractMonthlyClaimSystem/Models/HeroViewModel.cs
+++ ContractMonthlyClaimSystem/Models/HeroViewModel.cs
@@ -2,7 +2,7 @@ namespace ContractMonthlyClaimSystem.Models
 {
     public class HeroViewModel
     {
-        public string Title { get; set; } = "Contract Monthly Claim System";
+        public string Title { get; set; } = "Monthly Claim System";
         public string Description { get; set; } = "Submit monthly claims based on your hours lectured.";
         public string ButtonText { get; set; } = "My Dashboard";
     }
